6ef5e76fe00a84b108521cfa8f0837f12e9694d8
Mark Fantini <strictlybusinessemail242@gmail.com>
2019-03-23 20:38:58 -0600

    making progress on users database, creating the dispatch

--- src/store/actions/signup.js
+++ src/store/actions/signup.js
@@ -2,5 +2,17 @@ import { uiStartLoading, uiStopLoading } from './index'
 
 
 export const userSignup = userData => {
-
+    return dispatch => {
+        return fetch('https://beermo-1552602774929.firebaseio.com/users.json', {
+            method: "POST",
+            body: {
+                email: userData.email,
+                password: userData.password,
+                firstName: userData.firstName,
+                lastName: userData.lastName,
+                userName: userData.userName,
+                dateOfBirth: userData.dateOfBirth
+            }
+        })
+    }
 }
