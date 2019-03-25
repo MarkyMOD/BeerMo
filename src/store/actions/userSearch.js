e90922bd06a23b8446ac22f4780576b2c80c3701
Mark Fantini <strictlybusinessemail242@gmail.com>
2019-03-24 19:49:16 -0600

    added dispatch for userSearch action

--- src/store/actions/userSearch.js
+++ src/store/actions/userSearch.js
@@ -1,8 +1,17 @@
 import { USER_SEARCH } from './actionTypes'
 
+import { authGetToken } from './index'
+
 export const searchUsers = userName => {
     return dispatch => {
-        
+        let user = userName
+        dispatch(authGetToken())
+        .then(token => {
+            return fetch(`https://beermo-1552602774929.firebaseio.com/users.json?auth=${token}&orderBy="userName"&equalTo="${userName}"&print=pretty`)
+        })
+        .then(res => res.json())
+        .then(parsedRes => console.log(parsedRes))
+        .catch(err => console.log(err))
     }
 }
 
